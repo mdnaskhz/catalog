8c5362b1497306200b96ac695da40daca7447547
mdnaskhz <madinaaskhatkyzy01@gmail.com>
2025-12-10 18:38:26 +0500

    Update service.py

--- app/service.py
+++ app/service.py
@@ -1,1 +1,12 @@
+class CatalogService:
+    def __init__(self):
+        self.products = [
+            {"name": "Book", "price": 120, "category": "books"},
+            {"name": "Phone", "price": 500, "category": "electronics"},
+        ]
 
+    def get_products(self):
+        return self.products
+
+    def add_product(self, product: dict):
+        self.products.append(product)
