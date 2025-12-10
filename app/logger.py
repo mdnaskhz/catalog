661b46dc8079e1f5717af792ed50a6ee7bb13cba
mdnaskhz <madinaaskhatkyzy01@gmail.com>
2025-12-10 18:39:26 +0500

    Update logger.py

--- app/logger.py
+++ app/logger.py
@@ -1,1 +1,10 @@
+import logging
 
+logger = logging.getLogger("catalog")
+logger.setLevel(logging.INFO)
+
+file = logging.FileHandler("app.log")
+fmt = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file.setFormatter(fmt)
+
+logger.addHandler(file)
