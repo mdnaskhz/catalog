f56c2f00ee7d257288bceadf8b8b8aaed69cd786
mdnaskhz <madinaaskhatkyzy01@gmail.com>
2025-12-10 18:40:15 +0500

    Update test_api.py

--- test/test_api.py
+++ test/test_api.py
@@ -1,1 +1,14 @@
+from fastapi.testclient import TestClient
+from app.main import app
 
+client = TestClient(app)
+
+def test_get_products():
+    r = client.get("/products")
+    assert r.status_code == 200
+    assert isinstance(r.json(), list)
+
+
+def test_add_product():
+    r = client.post("/products", json={"name": "Pen", "price": 10, "category": "office"})
+    assert r.status_code == 200
